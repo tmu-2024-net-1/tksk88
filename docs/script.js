47908f817f847dec3eab16da8b170e72f5fdc2b5
tksk88 <132253269+tksk88@users.noreply.github.com>
2024-07-22 22:26:47 +0900

    7/22 22:26

--- docs/script.js
+++ docs/script.js
@@ -1,161 +1,178 @@
-    
-       
-       //ヘッダータイトル
-        var offsetX;
-        var offsetY;
-        var selectedImage = null;
-        var currentGroup = 0;
-        var buttonGroups;
-        var titles = [
-            "背景画像を作ろう",
-            "動物を配置しよう",
-            "エフェクトを追加しよう"
-        ];
+var offsetX;
+var offsetY;
+var selectedImage = null;
+var currentGroup = 0;
+var buttonGroups;
+var titles = [
+    "背景画像を作ろう",
+    "動物を配置しよう",
+    "エフェクトを追加しよう"
+];
 
-        function doInit() {
-            document.onmousemove = onMouseMove;
-            document.onmouseup = onMouseUp;
-            buttonGroups = document.querySelectorAll('.button-group');
-            showButtonGroup(currentGroup);
-            updateTitle(currentGroup);
+function doInit() {
+    document.onmousemove = onMouseMove;
+    document.onmouseup = onMouseUp;
+    buttonGroups = document.querySelectorAll('.button-group');
+    showButtonGroup(currentGroup);
+    updateTitle(currentGroup);
 
-            const switchImg = document.getElementById("switch");
-            switchImg.addEventListener("click", changeImg);
-        }
+    const switch1 = document.getElementById("switch1");
+    const switch2 = document.getElementById("switch2");
+    const switch3 = document.getElementById("switch3");
 
-        function showButtonGroup(index) {
-            buttonGroups.forEach((group, i) => {
-                group.classList.toggle('active', i === index);
-            });
-            document.getElementById('prevButton').style.display = index === 0 ? 'none' : 'inline-block';
-            document.getElementById('nextButton').style.display = index === buttonGroups.length - 1 ? 'none' : 'inline-block';
-        }
-
-        function nextGroup() {
-            if (currentGroup < buttonGroups.length - 1) {
-                currentGroup++;
-                showButtonGroup(currentGroup);
-                updateTitle(currentGroup);
-            }
-        }
-
-        function prevGroup() {
-            if (currentGroup > 0) {
-                currentGroup--;
-                showButtonGroup(currentGroup);
-                updateTitle(currentGroup);
-            }
-        }
-
-        function updateTitle(index) {
-            document.getElementById('dynamic-title').textContent = titles[index];
-        }
-
-        function onMouseDown(e) {
-            if (selectedImage) {
-                selectedImage.style.opacity = 1; 
-            }
-            selectedImage = this;
-            selectedImage.style.opacity = 0.5; 
-            offsetX = e.pageX - parseInt(this.style.left);
-            offsetY = e.pageY - parseInt(this.style.top);
-
-            activateSliders(this);
-
-            return false;
-        }
-
-        function onMouseMove(e) {
-            if (selectedImage) {
-                var newX = e.pageX - offsetX;
-                var newY = e.pageY - offsetY;
-
-                selectedImage.style.left = newX + 'px';
-                selectedImage.style.top = newY + 'px';
-
-                return false;
-            }
-        }
-
-        function onMouseUp(e) {
-            if (selectedImage) {
-                selectedImage.style.opacity = 1; // 選択解除時に元に戻す
-            }
-            selectedImage = null;
-        }
-
-        function showImages(type) {
-            var container = document.getElementById(type + "-container");
-            container.innerHTML = '';
-            var newImage = document.createElement("div");
-            newImage.className = "image-item";
-            newImage.style.top = "50%";
-            newImage.style.left = "50%";
-            newImage.style.transform = "translate(-50%, -50%)";
-            newImage.innerHTML = `
-                <img src="images/${type}.png" width="80px" height="80px" alt="${type}">
-            `;
-            newImage.onmousedown = onMouseDown;
-            container.appendChild(newImage);
-
-            var resizeXSlider = document.getElementById(type + "-resize-x");
-            var resizeYSlider = document.getElementById(type + "-resize-y");
-            var rotateSlider = document.getElementById(type + "-rotate");
-            resizeXSlider.value = 100;
-            resizeYSlider.value = 100;
-            rotateSlider.value = 0;
-            document.getElementById(type + "-scale-x").textContent = "100%";
-            document.getElementById(type + "-scale-y").textContent = "100%";
-            document.getElementById(type + "-rotation").textContent = "0°";
-
-            activateSliders(newImage);
-        }
-
-        function activateSliders(image) {
-            var type = image.querySelector("img").alt;
-
-            var resizeXSlider = document.getElementById(type + "-resize-x");
-            var resizeYSlider = document.getElementById(type + "-resize-y");
-            var rotateSlider = document.getElementById(type + "-rotate");
-
-            resizeXSlider.oninput = function () {
-                var scaleX = this.value / 100;
-                var img = image.querySelector("img");
-                img.style.width = 80 * scaleX + 'px';
-                document.getElementById(type + "-scale-x").textContent = this.value + "%";
-            };
-
-            resizeYSlider.oninput = function () {
-                var scaleY = this.value / 100;
-                var img = image.querySelector("img");
-                img.style.height = 80 * scaleY + 'px';
-                document.getElementById(type + "-scale-y").textContent = this.value + "%";
-            };
-
-            rotateSlider.oninput = function () {
-                var rotation = this.value;
-                image.style.transform = `translate(-50%, -50%) rotate(${rotation}deg)`;
-                document.getElementById(type + "-rotation").textContent = rotation + "°";
-            };
-        }
-
-        function changeImg() {
-    const switchImg = document.getElementById("switch");
-    switchImg.classList.toggle("on_switch");
-
-    const types = ["musi", "tori", "sakana"];
-    types.forEach(type => {
-        var img = document.querySelector(`#${type}-container .image-item img`);
-        if (img) {
-            var isGif = img.src.endsWith(`${type}.gif`);
-            var newSrc = isGif ? `images/${type}.png` : `images/${type}.gif`;
-            img.src = newSrc;
-        }
-    });
-    window.addEventListener("load", function() {
-            doInit();
-        });
+    switch1.addEventListener("click", () => changeImg("light1"));
+    switch2.addEventListener("click", () => changeImg("light2"));
+    switch3.addEventListener("click", () => changeImg("light3"));
 }
+
+function showButtonGroup(index) {
+    buttonGroups.forEach((group, i) => {
+        group.classList.toggle('active', i === index);
+    });
+    document.getElementById('prevButton').style.display = index === 0 ? 'none' : 'inline-block';
+    document.getElementById('nextButton').style.display = index === buttonGroups.length - 1 ? 'none' : 'inline-block';
+}
+
+function nextGroup() {
+    if (currentGroup < buttonGroups.length - 1) {
+        currentGroup++;
+        showButtonGroup(currentGroup);
+        updateTitle(currentGroup);
+    }
+}
+
+function prevGroup() {
+    if (currentGroup > 0) {
+        currentGroup--;
+        showButtonGroup(currentGroup);
+        updateTitle(currentGroup);
+    }
+}
+
+function updateTitle(index) {
+    document.getElementById('dynamic-title').textContent = titles[index];
+}
+
+function onMouseDown(e) {
+    if (selectedImage) {
+        selectedImage.style.opacity = 1; 
+    }
+    selectedImage = this;
+    selectedImage.style.opacity = 0.5; 
+    offsetX = e.pageX - parseInt(this.style.left);
+    offsetY = e.pageY - parseInt(this.style.top);
+
+    activateSliders(this);
+
+    return false;
+}
+
+function onMouseMove(e) {
+    if (selectedImage) {
+        var newX = e.pageX - offsetX;
+        var newY = e.pageY - offsetY;
+
+        selectedImage.style.left = newX + 'px';
+        selectedImage.style.top = newY + 'px';
+
+        return false;
+    }
+}
+
+function onMouseUp(e) {
+    if (selectedImage) {
+        selectedImage.style.opacity = 1; // 選択解除時に元に戻す
+    }
+    selectedImage = null;
+}
+
+function showImages(type) {
+    var container = document.getElementById(type + "-container");
+    container.innerHTML = '';
+    var newImage = document.createElement("div");
+    newImage.className = "image-item";
+    newImage.style.top = "50%";
+    newImage.style.left = "50%";
+    newImage.style.transform = "translate(-50%, -50%)";
+    newImage.innerHTML = `
+        <img src="images/${type}.png" width="100px" height="100px" alt="${type}">
+    `;
+    newImage.onmousedown = onMouseDown;
+    container.appendChild(newImage);
+
+    var resizeXSlider = document.getElementById(type + "-resize-x");
+    var resizeYSlider = document.getElementById(type + "-resize-y");
+    var rotateSlider = document.getElementById(type + "-rotate");
+    resizeXSlider.value = 100;
+    resizeYSlider.value = 100;
+    rotateSlider.value = 0;
+    document.getElementById(type + "-scale-x").textContent = "100%";
+    document.getElementById(type + "-scale-y").textContent = "100%";
+    document.getElementById(type + "-rotation").textContent = "0°";
+
+    activateSliders(newImage);
+}
+
+function activateSliders(image) {
+    var type = image.querySelector("img").alt;
+
+    var resizeXSlider = document.getElementById(type + "-resize-x");
+    var resizeYSlider = document.getElementById(type + "-resize-y");
+    var rotateSlider = document.getElementById(type + "-rotate");
+
+    resizeXSlider.oninput = function () {
+        var scaleX = this.value / 100;
+        var img = image.querySelector("img");
+        img.style.width = 80 * scaleX + 'px';
+        document.getElementById(type + "-scale-x").textContent = this.value + "%";
+    };
+
+    resizeYSlider.oninput = function () {
+        var scaleY = this.value / 100;
+        var img = image.querySelector("img");
+        img.style.height = 80 * scaleY + 'px';
+        document.getElementById(type + "-scale-y").textContent = this.value + "%";
+    };
+
+    rotateSlider.oninput = function () {
+        var rotation = this.value;
+        image.style.transform = `translate(-50%, -50%) rotate(${rotation}deg)`;
+        document.getElementById(type + "-rotation").textContent = rotation + "°";
+    };
+}
+
+document.getElementById("switch1").addEventListener("click", function() {
+    toggleSwitch("musi", this);
+});
+
+document.getElementById("switch2").addEventListener("click", function() {
+    toggleSwitch("tori", this);
+});
+
+document.getElementById("switch3").addEventListener("click", function() {
+    toggleSwitch("sakana", this);
+});
+
+function toggleSwitch(type, element) {
+    var imgElement = document.getElementById(type + "-container").querySelector("img");
+    if (imgElement) {
+        var currentSrc = imgElement.getAttribute("src");
+        if (currentSrc.includes(type + ".png")) {
+            imgElement.setAttribute("src", "images/" + type + ".gif");
+        } else {
+            imgElement.setAttribute("src", "images/" + type + ".png");
+        }
+    }
+}
+$("#switch1").on("click", function () {
+    $(this).toggleClass("change");
+  });
+  $("#switch2").on("click", function () {
+    $(this).toggleClass("change");
+  });
+  $("#switch3").on("click", function () {
+    $(this).toggleClass("change");
+  });
 
 //雲アニメーション
         var animationInterval;
